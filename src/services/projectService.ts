d2b2f94d90d5413034592befaba72986330ad505
MartinP460 <martin.polley460gmail.com>
2022-07-30 12:00:04 +0200

    Create project blog service.

---
+++ src/services/projectService.ts
@@ -0,0 +1,59 @@
+import fs from 'fs'
+import { join } from 'path'
+import matter from 'gray-matter'
+
+const projectsDirectory = join(process.cwd(), '/projects')
+
+const getPostSlugs = () => {
+  return fs.readdirSync(projectsDirectory)
+}
+
+const getProject = (slug: string, fields: string[] = []) => {
+  const realSlug = slug.replace(/\.md$/, '')
+  const fullPath = join(projectsDirectory, `${realSlug}.md`)
+  const fileContents = fs.readFileSync(fullPath, 'utf8')
+  const { data, content } = matter(fileContents)
+
+  const items: { [key: string]: string | number } = {}
+
+  for (const field of fields) {
+    if (typeof data[field] !== undefined) {
+      items[field] = data[field]
+    }
+
+    if (field === 'content') {
+      items[field] = content
+    }
+  }
+
+  return items
+}
+
+export const getAllProjectsData = () => {
+  const slugs = getPostSlugs()
+  const projects = slugs
+    .map((slug) =>
+      getProject(slug, [
+        'title',
+        'intro',
+        'coverImage',
+        'liveUrl',
+        'repoUrl',
+        'priority'
+      ])
+    )
+    .sort((a, b) => Number(a.priority) - Number(b.priority))
+
+  return projects
+}
+
+export const getProjectBySlug = (slug: string) => {
+  return getProject(slug, [
+    'title',
+    'intro',
+    'coverImage',
+    'liveUrl',
+    'repoUrl',
+    'content'
+  ])
+}
---
+++ src/types/project.ts
@@ -0,0 +1,8 @@
+export type Project = {
+  title: string
+  intro: string
+  coverImage: string
+  liveUrl: string
+  repoUrl: string
+  priority: 1 | 2 | 3
+}
